893f4b12f759c4a469ddb2ecaea99c6fe0bb1724
Max Rayner <maxrayner451@gmail.com>
2026-09-18 21:46:38 +0100

    Finished rough draft of security checker, few bugs to fix

--- password-security-checker/main.py
+++ password-security-checker/main.py
@@ -11,43 +11,83 @@ def getPassword():
     return password
 
 def checkLength(password):
-    if len(password) >= 16:
-        return True
-    return False
+    length = len(password)
+    if length < 8: 
+        return "Weak", "X", 0
+    elif length < 12:
+        return "Medium", "X", 1
+    elif length < 16: 
+        return "Strong", "✓", 2
+    else:
+        return "Very Strong", "✓", 3
+
 
 def checkUpper(password):
     for char in password:
         if char.isupper():
-            return True
-    return False
+            return "Yes", "✓"
+    return "No", "X"
 
 def checkLower(password):
     for char in password:
         if char.islower():
-            return True
-    return False
+            return "Yes", "✓"
+    return "No", "X"
 
 
 def checkSpecialChar(password):
     for char in password:
         if char in string.punctuation:
-            return True
-    return False
+            return "Yes", "✓"
+    return "No", "X"
 
 
 def checkNumbers(password):
     for char in password:
         if char in string.digits:
-            return True
-    return False
-    
-def mainChecks():
-    password = getPassword()
-    correctLength = checkLength(password)
-    correctUpper = checkUpper(password)
-    correctLower = checkLower(password)
-    correctSpecial = checkSpecialChar(password)
-    correctNumber = checkNumbers(password)
-    print(correctLength,correctUpper,correctLower,correctSpecial,correctNumber)
+            return "Yes", "✓"
+    return "No", "X"
 
-mainChecks()
+def checkCommon(password,FILENAME):
+    try:
+        with open(FILENAME) as f:
+            for lines in f:
+                if password == lines.strip():
+                    return "Yes", "X"
+        return "No", "✓" 
+    except:
+        print("ERROR: File not found")
+
+def mainOutput():
+    print(f"{' PASSWORD SECURITY CHECKER ':=^45}")
+    print("")
+
+    password = getPassword()
+    correctLength, lengthMarker, overallScore = checkLength(password)
+    correctUpper, upperMarker = checkUpper(password)
+    correctLower, lowerMarker = checkLower(password)
+    correctSpecial, specialMarker = checkSpecialChar(password)
+    correctNumber, numberMarker = checkNumbers(password)
+    correctCommon, commonMarker = checkCommon(password, FILENAME)
+
+    overallList = [upperMarker,lowerMarker,specialMarker,numberMarker,commonMarker]
+    for field in overallList:
+        if field == "✓":
+            overallScore += 1
+
+
+    print(f" \nPassword Analysis\n{"-"*24}")
+    output = "{:<24} {:^} {:>}"
+
+    print(output.format("Length:",lengthMarker,correctLength))
+    print(output.format("Uppercase Letters:",upperMarker,correctUpper))
+    print(output.format("Lowercase Letters:",lowerMarker,correctLower))
+    print(output.format("Special Characters:",specialMarker,correctSpecial))
+    print(output.format("Numbers:",numberMarker,correctNumber))
+    print(output.format("Common Password:",commonMarker,correctCommon))
+    
+    print(f"\nOverall Analysis: {overallScore}/10")
+
+
+FILENAME = "common_passwords.txt"
+mainOutput()
