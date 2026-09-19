33ffb7613e72f1a3addc2afbb127787a322f8303
Max Rayner <maxrayner451@gmail.com>
2026-09-19 14:16:02 +0100

    Added exception handling

--- Basic-Password-Cracker/password.py
+++ Basic-Password-Cracker/password.py
@@ -1,153 +1,186 @@
 import hashlib
 
 def common_passwords():
-    print("First")
+    print("Checking...")
     user_hash_dict = {}
-    with open("common_passwords.txt", "r", encoding="utf-8") as file:
-        common_passwords = file.read().splitlines()
-    with open("passwords.txt", "r", encoding="utf-8") as file:
-        text = file.read().splitlines()
-        for user_hash in text:
-            username = user_hash.split(":")[0]
-            hash = user_hash.split(":")[1]
-            user_hash_dict[username] = hash
+    try:
+        with open("common_passwords.txt", "r", encoding="utf-8") as file:
+            common_passwords = file.read().splitlines()
+        with open("passwords.txt", "r", encoding="utf-8") as file:
+            text = file.read().splitlines()
+            for user_hash in text:
+                username = user_hash.split(":")[0]
+                hash = user_hash.split(":")[1]
+                user_hash_dict[username] = hash
 
-    for password in common_passwords:
-        hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
-        for username, hash in user_hash_dict.items():
-            if hashed_password == hash:
-                print(f"HASH FOUND\n{username} : {password}")
+        for password in common_passwords:
+            hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+            for username, hash in user_hash_dict.items():
+                if hashed_password == hash:
+                    print(f"HASH FOUND\n{username} : {password}")
+    except FileNotFoundError:
+        print("ERROR: File not found")
+
 
 def common_passwords2():
-    print("Second")
+    print("Checking...")
     user_hash_dict = {}
-    with open("common_passwords2.txt", "r", encoding="utf-8") as file:
-        common_passwords = file.read().splitlines()
-    with open("passwords.txt", "r", encoding="utf-8") as file:
-        text = file.read().splitlines()
-        for user_hash in text:
-            username = user_hash.split(":")[0]
-            hash = user_hash.split(":")[1]
-            user_hash_dict[username] = hash
+    try:
+        with open("common_passwords2.txt", "r", encoding="utf-8") as file:
+            common_passwords = file.read().splitlines()
 
-    for password in common_passwords:
-        hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
-        for username, hash in user_hash_dict.items():
-            if hashed_password == hash:
-                print(f"HASH FOUND\n{username} : {password}")
+        with open("passwords.txt", "r", encoding="utf-8") as file:
+            text = file.read().splitlines()
+            for user_hash in text:
+                username = user_hash.split(":")[0]
+                hash = user_hash.split(":")[1]
+                user_hash_dict[username] = hash
+
+        for password in common_passwords:
+                hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+                for username, hash in user_hash_dict.items():
+                    if hashed_password == hash:
+                        print(f"HASH FOUND\n{username} : {password}")
+    except FileNotFoundError:
+        print("ERROR: File not found")
+        
 
 def common_passwords3():
-    print("Third")
+    print("Checking...")
     user_hash_dict = {}
-    with open("common_passwords3.txt", "r", encoding="utf-8") as file:
-        common_passwords = file.read().splitlines()
-    with open("passwords.txt", "r", encoding="utf-8") as file:
-        text = file.read().splitlines()
-        for user_hash in text:
-            username = user_hash.split(":")[0]
-            hash = user_hash.split(":")[1]
-            user_hash_dict[username] = hash
+    try: 
+        with open("common_passwords3.txt", "r", encoding="utf-8") as file:
+            common_passwords = file.read().splitlines()
+        with open("passwords.txt", "r", encoding="utf-8") as file:
+            text = file.read().splitlines()
+            for user_hash in text:
+                username = user_hash.split(":")[0]
+                hash = user_hash.split(":")[1]
+                user_hash_dict[username] = hash
 
-    for password in common_passwords:
-        hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
-        for username, hash in user_hash_dict.items():
-            if hashed_password == hash:
-                print(f"HASH FOUND\n{username} : {password}")
+        for password in common_passwords:
+            hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+            for username, hash in user_hash_dict.items():
+                if hashed_password == hash:
+                    print(f"HASH FOUND\n{username} : {password}")
+    except FileNotFoundError:
+        print("ERROR: File not found")
 
 def books_lower():
-    print("Fourth")
+    print("Checking...")
     user_hash_dict = {}
-    with open("greatest_books_of_all_time_lowercase.txt", "r", encoding="utf-8") as file:
-        common_passwords = file.read().splitlines()
-    with open("passwords.txt", "r", encoding="utf-8") as file:
-        text = file.read().splitlines()
-        for user_hash in text:
-            username = user_hash.split(":")[0]
-            hash = user_hash.split(":")[1]
-            user_hash_dict[username] = hash
+    try:
+        with open("greatest_books_of_all_time_lowercase.txt", "r", encoding="utf-8") as file:
+            common_passwords = file.read().splitlines()
+        with open("passwords.txt", "r", encoding="utf-8") as file:
+            text = file.read().splitlines()
+            for user_hash in text:
+                username = user_hash.split(":")[0]
+                hash = user_hash.split(":")[1]
+                user_hash_dict[username] = hash
 
-    for password in common_passwords:
-        hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
-        for username, hash in user_hash_dict.items():
-            if hashed_password == hash:
-                print(f"HASH FOUND\n{username} : {password}")
+        for password in common_passwords:
+            hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+            for username, hash in user_hash_dict.items():
+                if hashed_password == hash:
+                    print(f"HASH FOUND\n{username} : {password}")
+
+    except FileNotFoundError:
+        print("ERROR: File not found")
 
 def books_orig():
-    print("Fifth")
+    print("Checking...")
     user_hash_dict = {}
-    with open("greatest_books_of_all_time_original.txt", "r", encoding="utf-8") as file:
-        common_passwords = file.read().splitlines()
-    with open("passwords.txt", "r", encoding="utf-8") as file:
-        text = file.read().splitlines()
-        for user_hash in text:
-            username = user_hash.split(":")[0]
-            hash = user_hash.split(":")[1]
-            user_hash_dict[username] = hash
+    try:
+        with open("greatest_books_of_all_time_original.txt", "r", encoding="utf-8") as file:
+            common_passwords = file.read().splitlines()
+        with open("passwords.txt", "r", encoding="utf-8") as file:
+            text = file.read().splitlines()
+            for user_hash in text:
+                username = user_hash.split(":")[0]
+                hash = user_hash.split(":")[1]
+                user_hash_dict[username] = hash
 
-    for password in common_passwords:
-        hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
-        for username, hash in user_hash_dict.items():
-            if hashed_password == hash:
-                print(f"HASH FOUND\n{username} : {password}")
+        for password in common_passwords:
+            hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+            for username, hash in user_hash_dict.items():
+                if hashed_password == hash:
+                    print(f"HASH FOUND\n{username} : {password}")
+
+    except FileNotFoundError:
+        print("ERROR: File not found")
 
 def books_upper():
-    print("Sixth")
+    print("Checking...")
     user_hash_dict = {}
-    with open("greatest_books_of_all_time_uppercase.txt", "r", encoding="utf-8") as file:
-        common_passwords = file.read().splitlines()
-    with open("passwords.txt", "r", encoding="utf-8") as file:
-        text = file.read().splitlines()
-        for user_hash in text:
-            username = user_hash.split(":")[0]
-            hash = user_hash.split(":")[1]
-            user_hash_dict[username] = hash
+    try:
+        with open("greatest_books_of_all_time_uppercase.txt", "r", encoding="utf-8") as file:
+            common_passwords = file.read().splitlines()
+        with open("passwords.txt", "r", encoding="utf-8") as file:
+            text = file.read().splitlines()
+            for user_hash in text:
+                username = user_hash.split(":")[0]
+                hash = user_hash.split(":")[1]
+                user_hash_dict[username] = hash
 
-    for password in common_passwords:
-        hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
-        for username, hash in user_hash_dict.items():
-            if hashed_password == hash:
-                print(f"HASH FOUND\n{username} : {password}")
+        for password in common_passwords:
+            hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+            for username, hash in user_hash_dict.items():
+                if hashed_password == hash:
+                    print(f"HASH FOUND\n{username} : {password}")
+
+    except FileNotFoundError:
+        print("ERROR: File not found")
 
 def books_var():
-    print("Seventh")
+    print("Checking...")
     user_hash_dict = {}
-    with open("greatest_books_of_all_time_with_leet_variations.txt", "r", encoding="utf-8") as file:
-        common_passwords = file.read().splitlines()
-    with open("passwords.txt", "r", encoding="utf-8") as file:
-        text = file.read().splitlines()
-        for user_hash in text:
-            username = user_hash.split(":")[0]
-            hash = user_hash.split(":")[1]
-            user_hash_dict[username] = hash
+    try:
+        with open("greatest_books_of_all_time_with_leet_variations.txt", "r", encoding="utf-8") as file:
+            common_passwords = file.read().splitlines()
+        with open("passwords.txt", "r", encoding="utf-8") as file:
+            text = file.read().splitlines()
+            for user_hash in text:
+                username = user_hash.split(":")[0]
+                hash = user_hash.split(":")[1]
+                user_hash_dict[username] = hash
 
-    for password in common_passwords:
-        hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
-        for username, hash in user_hash_dict.items():
-            if hashed_password == hash:
-                print(f"HASH FOUND\n{username} : {password}")
-
+        for password in common_passwords:
+            hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+            for username, hash in user_hash_dict.items():
+                if hashed_password == hash:
+                    print(f"HASH FOUND\n{username} : {password}")
+    except FileNotFoundError:
+        print("ERROR: File not found")
 def japanese_common():
-    print("Eighth")
+    print("Checking...")
     user_hash_dict = {}
-    with open("japanese_common.txt", "r", encoding="utf-8") as file:
-        common_passwords = file.read().splitlines()
-    with open("passwords.txt", "r", encoding="utf-8") as file:
-        text = file.read().splitlines()
-        for user_hash in text:
-            username = user_hash.split(":")[0]
-            hash = user_hash.split(":")[1]
-            user_hash_dict[username] = hash
+    try:
+        with open("japanese_common.txt", "r", encoding="utf-8") as file:
+            common_passwords = file.read().splitlines()
+        with open("passwords.txt", "r", encoding="utf-8") as file:
+            text = file.read().splitlines()
+            for user_hash in text:
+                username = user_hash.split(":")[0]
+                hash = user_hash.split(":")[1]
+                user_hash_dict[username] = hash
 
-    for password in common_passwords:
-        hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
-        for username, hash in user_hash_dict.items():
-            if hashed_password == hash:
-                print(f"HASH FOUND\n{username} : {password}")
-common_passwords()
-common_passwords2()
-common_passwords3()
-books_lower()
-books_orig()
-books_upper()
-books_var()     
-japanese_common()    
+        for password in common_passwords:
+            hashed_password = hashlib.sha256(password.encode("utf-8")).hexdigest()
+            for username, hash in user_hash_dict.items():
+                if hashed_password == hash:
+                    print(f"HASH FOUND\n{username} : {password}")
+    except FileNotFoundError:
+        print("ERROR: File not found")
+
+def main():
+    common_passwords()
+    common_passwords2()
+    common_passwords3()
+    books_lower()
+    books_orig()
+    books_upper()
+    books_var()     
+    japanese_common()    
+
+main()
