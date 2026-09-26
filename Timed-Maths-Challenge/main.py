48b5cb220b812a6f3068565e56e214d427c0280d
Max Rayner <157379775+maxrayner@users.noreply.github.com>
2026-09-26 14:48:29 +0100

    Add multiplication feature to timed maths challenge -> displays are just for debugging
    Implemented a timed mental maths challenge with multiplication functionality, including input validation and response checking.

--- Timed-Maths-Challenge/main.py
+++ Timed-Maths-Challenge/main.py
@@ -1,1 +1,32 @@
+# timed mental maths challenge code
+# Defaults: Multiplication (2,100) + (2,100), Division is same but reversed (only integers allowed), Addition (2,100) (2, 100), Subtraction (2,100) (2,100)
 
+from random import randint
+
+def multiplication(minimum=2,maximum=100):
+    firstMult, secondMult = randint(minimum,maximum), randint(minimum,maximum)
+    answer = firstMult * secondMult
+    return firstMult, secondMult, "*", answer
+
+def askResponse(first,second,symbol):
+    while True:
+        try:
+            return int(input(f"{first} {symbol} {second}: "))
+        except ValueError:
+            print("ERROR: Not an integer")
+
+def checkResponse(response,answer):
+    if response == answer:
+        return True
+    return False
+
+def main():
+    first, second, symbol, answer = multiplication()
+    response = askResponse(first,second,symbol)
+    correct = checkResponse(response, answer)
+    if correct:
+        print("You got it correct")
+    else:
+        print(f"You got it wrong the answer was {answer}")
+
+main()
